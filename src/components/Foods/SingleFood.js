a0cd0b992e46a7b937598dbf4b761edff3978d8f
Richard T <richard.tambong04@gmail.com>
2023-06-07 01:51:47 +0800

    update spinner

--- src/components/Foods/SingleFood.js
+++ src/components/Foods/SingleFood.js
@@ -12,7 +12,8 @@ export const SingleFood = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `https://api.edamam.com/api/recipes/v2/recipe_${params.id}?type=public&&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`);
+          `https://api.edamam.com/api/recipes/v2/recipe_${params.id}?type=public&&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`
+        );
 
         if (!response.ok) {
           throw new Error("Failed to fetch recipes");
@@ -31,34 +32,52 @@ export const SingleFood = () => {
 
   return (
     <>
-      <div class="container px-4 py-8 mx-auto">
-        <div class="flex flex-col md:flex-row">
-          <div class="bg-white p-8 mb-4 md:mb-0">
-            <h1 className="text-2xl font-bold mb-4">{recipes.label}</h1>
+      <div class="container px-6 py-8 mx-auto">
+
+      <div className="relative flex py-5 items-center">
+          <div className="flex-grow border-t border-gray-400"></div>
+          <span className="flex-shrink text-3xl poppins mx-5 text-gray-500">
+            Recipe Details
+          </span>
+          <div className="flex-grow border-t border-gray-400"></div>
+        </div>
+        <h5 className="poppins text-gray-500 pb-5 text-center">
+          Explore the Top 20 Recipes from around the globe.
+        </h5>
+        <h1 className="text-2xl font-bold mb-4 text-center">{recipes.label}</h1>
+
+        <div class="flex flex-col md:flex-row murk-solid-border">
+          <div class="bg-white p-8 md:mb-0">
             <div className="food-pic-full">
-              <img src={recipes.image} alt="recipe" className="w-full rounded-lg food-pic-md" />
+              <img
+                src={recipes.image}
+                alt="recipe"
+                className="w-full rounded-lg food-pic-md"
+              />
             </div>
           </div>
 
-          <div class="bg-white p-8">
+
+          <div class="bg-red-300 p-8">
             <h2 className="text-xl font-semibold mb-2">Ingredients :</h2>
             <ul className="list-disc list-inside">
               <li>Ingredients 1</li>
-              
             </ul>
             <h2 className="text-xl font-semibold mt-4 mb-2">Instructions :</h2>
             <ol className="list-decimal list-inside">
               <li>Step 1</li>
             </ol>
           </div>
-
         </div>
-
+        
         <div class="flex flex-col md:flex-row">
           <p>{recipes.ingredientLines}</p>
         </div>
 
+      
       </div>
+
+      
     </>
   );
 };
--- src/pages/Playground.js
+++ src/pages/Playground.js
@@ -1,16 +1,10 @@
-import { NavigationBar } from "../components/Navbar/NavigationBar";
-import {MainHeader} from "../components/Header/MainHeader";
-import {ChefDetails} from "../components/Chefs/ChefDetails";
-import { Footer } from "../components/Footer/Footer";
+
 
 export const Playground = () => {
 
   return (
     <>
-    <NavigationBar />
-    <MainHeader />
-    <ChefDetails />
-    <Footer />
+   
     </>
   );
 };
